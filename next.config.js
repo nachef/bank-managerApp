0e258b9e6fd0888566a14a2cbc3b707be60a7522
nachef <nathanvic.2001@gmail.com>
2023-01-12 00:59:00 -0300

    fix/ svg icons

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  pageExtensions: ["page.tsx", ".page.ts", "page.jsx", "page.js"],
+  compiler: {
+    styledComponents: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ["i.imgur.com"],
+  },
+  env: {
+    API: process.env.API,
+  },
+  webpack: (config) => {
+    config.module.rules.push(
+      {
+        test: /\.(eot|ttf|woff|woff2|mp4|pdf|webm|txt)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "static/chunks/[path][name].[hash][ext]",
+        },
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ["@svgr/webpack"],
+      }
+    );
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
