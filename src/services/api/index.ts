74c2be0d839740e0bb2afb8e7ff0fa23a524d37d
nachef <nathanvic.2001@hotmail.com>
2023-03-14 22:20:02 -0300

    fix/done

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/",
+  baseURL: "https://backend-deploy-vtht.onrender.com/",
   timeout: 15000,
   headers: {
     "Access-Control-Allow-Origin": "*",
